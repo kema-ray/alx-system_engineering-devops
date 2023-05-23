425ffe112d12bcdf24e14678351f1dc30100c8fb
kema-ray <rachelkemuma99@gmail.com>
2023-05-23 05:38:37 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,32 +3,21 @@
 Python script that uses REST API, for a given employee ID, returns
 information about his/her TODO list progress
 """
-import re
 import requests
 import sys
-
 
 API = "https://jsonplaceholder.typicode.com/"
 """
 REST API url
 """
 
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    user = requests.get(API + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(API + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [todo.get("title")
+    for todo in todos 
+    if todo.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(complete)) for complete in completed]
